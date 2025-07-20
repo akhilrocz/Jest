51ed87762c79053137b6f217dd5866e347aef2fa
Akhil roczz <b.akhil667@gmail.com>
2025-07-20 19:33:45 +0530

    updated main.js and main.test.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
-function sum(a,b){
-    if(isNaN(a) || isNaN(b)){
-        return;
-    }
-    return a+b;
+function sum(a, b) {
+  if (isNaN(a) || isNaN(b)) {
+    return;
+  }
+  return a + b;
 }
 
-
-function diff(a,b){
-    return a-b;
+function diff(a, b) {
+  return a - b;
 }
 
+function isEven(num) {
+  return num % 2 === 0;
+}
 
-module.exports =sum;
+function getUser() {
+  return { id: 1, name: "Akhil" };
+}
+
+module.exports = { sum, diff, isEven, getUser };
--- src/main.test.js
+++ src/main.test.js
@@ -1,4 +1,4 @@
-const sum = require("./main");
+const { sum, diff, isEven, getUser } = require("./main");
 
 describe("test sum function", () => {
   test("adds 1+2 returns 3", () => {
@@ -7,5 +7,21 @@ describe("test sum function", () => {
 
   test('adds 1 + "hello" to return undefined', () => {
     expect(sum(1, "hello")).toBeUndefined();
+  });
+
+  test("subtracts 5 - 3 equals 2", () => {
+    expect(diff(5, 3)).toBe(2);
+  });
+
+  test("checks if 4 is even", () => {
+    expect(isEven(4)).toBeTruthy();
+  });
+
+  test("returns user object with name Akhil", () => {
+    expect(getUser()).toEqual({ id: 1, name: "Akhil" });
+  });
+
+  test("sum of 2 and 3 not equal to 10", () => {
+    expect(sum(2, 3)).not.toBe(10);
   });
 });
